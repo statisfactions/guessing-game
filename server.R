8bd0c1982db917d59bc09cebd8615a743e00a3d7
Ethan Brown <ethancbrown@gmail.com>
2014-01-23 19:19:10 -0600

    bare-bones working in base R

--- server.R
+++ server.R
@@ -29,11 +29,29 @@ shinyServer(function(input, output, session) {
                 prediction$color[curr] <<- input$color
                 updateRadioButtons(session, "color", "Prediction of Next Color", c('(Please select an option below)' = 'none', 'Dark Red' = 'red', 'Light Green' = 'green'), selected = '(Please select an option below)')
             })
-        return(rbind(actual, prediction))
+        return(list(actual = actual, prediction = prediction))
     })
 
     output$predictionplot <- renderPlot({
-        plot(c(1,50), 0:1, type = "n")
+        all <- county()
+         
+        plot(c(1,51), 0:1, type = "n")
+
+        redpred <- all$prediction$number[prediction$color %in% "red"]
+        redact <-  all$actual$number[actual$color %in% "red"]
+
+        greenpred <- all$prediction$number[prediction$color %in% "green"]
+        greenact <-  all$actual$number[actual$color %in% "green"]
+
+        if(length(redpred) > 0)
+            rect(redpred, 0.5, redpred+1, 1, col = "dark red")
+        if(length(redact) > 0)
+            rect(redact, 0, redact+1, 0.5,  col = "dark red")
+        if(length(greenpred) > 0)
+            rect(greenpred, 0.5, greenpred+1, 1,  col = "light green")
+        if(length(greenact) > 0)
+            rect(greenact, 0, greenact+1, 0.5,  col = "light green")
+        
     }, width = 650, height = 400)
 
     })
