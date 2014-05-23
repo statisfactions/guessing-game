f4f8b10afa318b9a4de393c72ef4f991c9e03549
Ethan Brown <ethancbrown@gmail.com>
2014-05-23 11:13:55 -0500

    creating animation and shinyapps file

---
+++ create-animation.R
@@ -0,0 +1,35 @@
+
+    dat <-  sample(c(FALSE, FALSE, FALSE, TRUE), size = 50, replace = T)
+
+
+
+for(i in dat) {
+    dev.hold()
+    plot(0:1, 0:1, type = "n", axes = F, xlab = NA, ylab = NA)
+    ani.pause(interv * 0.3)
+    if(!i)
+        rect(0, 0, 1, 1, col = "light green")
+    else
+        rect(0, 0, 1, 1, col = "dark red")
+    ani.pause(interv * 0.7)
+}
+dev.off()
+
+
+
+interv <- 0.1
+
+saveGIF({
+for(i in dat) {
+    dev.hold()
+    plot(0:1, 0:1, type = "n", axes = F, xlab = NA, ylab = NA)
+    ani.pause(interv * 0.3)
+    plot(0:1, 0:1, type = "n", axes = F, xlab = NA, ylab = NA)
+    ani.pause(interv * 0.3)
+    if(!i)
+        rect(0, 0, 1, 1, col = "light green")
+    else
+        rect(0, 0, 1, 1, col = "dark red")
+    ani.pause(interv * 0.7)
+}
+}, movie.name = "animation.gif", clean = FALSE)
