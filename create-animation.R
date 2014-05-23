c3ff257c0e0b0c484c0f56d69a1f04f02164f0c2
Ethan Brown <ethancbrown@gmail.com>
2014-05-23 11:33:15 -0500

    add README and animation.gif

--- create-animation.R
+++ create-animation.R
@@ -1,21 +1,6 @@
+library(animation)
 
-    dat <-  sample(c(FALSE, FALSE, FALSE, TRUE), size = 50, replace = T)
-
-
-
-for(i in dat) {
-    dev.hold()
-    plot(0:1, 0:1, type = "n", axes = F, xlab = NA, ylab = NA)
-    ani.pause(interv * 0.3)
-    if(!i)
-        rect(0, 0, 1, 1, col = "light green")
-    else
-        rect(0, 0, 1, 1, col = "dark red")
-    ani.pause(interv * 0.7)
-}
-dev.off()
-
-
+dat <-  sample(c(FALSE, FALSE, FALSE, TRUE), size = 50, replace = T)
 
 interv <- 0.1
 
@@ -32,4 +17,4 @@ for(i in dat) {
         rect(0, 0, 1, 1, col = "dark red")
     ani.pause(interv * 0.7)
 }
-}, movie.name = "animation.gif", clean = FALSE)
+}, movie.name = "animation.gif")
